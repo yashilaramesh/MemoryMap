40c1e2f0fd00932d7dd433abcfba344cc3a09d5b
Yashila Ramesh <yashiramesh1122@gmail.com>
2025-03-30 17:05:22 -0400

    maps and accounts

--- MemoryMap/urls.py
+++ MemoryMap/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('accounts.urls')),
-    path("admin/", admin.site.urls),
+    path('', include('maps.urls')),
+    path('admin/', admin.site.urls),
 ]
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from django.contrib import admin
 from django.urls import include, path
 
@@ -5,3 +6,11 @@ urlpatterns = [
     path('', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ]
+=======
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('accounts/', views.accounts, name='accounts'),
+]
+>>>>>>> 4f22921 (maps and accounts)
