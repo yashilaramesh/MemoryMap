a767066fcd5d335d26f60675ad9a80f085e62a8c
Yashila Ramesh <yashiramesh1122@gmail.com>
2025-03-30 17:07:42 -0400

    maps and accounts 2

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,16 +1,5 @@
-<<<<<<< HEAD
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('', include('accounts.urls')),
-    path('admin/', admin.site.urls),
-]
-=======
 from django.urls import path
 from . import views
-
 urlpatterns = [
     path('accounts/', views.accounts, name='accounts'),
 ]
->>>>>>> 4f22921 (maps and accounts)
