7fccda5b0a95b617f630a64c153800c0e0ebe723
racheljohn05 <racheljohn556@gmail.com>
2025-04-06 17:47:42 -0400

    attempt at adding memories to admin

--- memories/models.py
+++ memories/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+class Memory(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    date = models.IntegerField()
+    description = models.TextField()
+    #image = models.ImageField(upload_to='movie_images/')
+    def __str__(self):
+        return str(self.id) + ' - ' + self.title
 # Create your models here.
