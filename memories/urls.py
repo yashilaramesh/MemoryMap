7e3849bd8efdabfb0ed70cafd9ddc9c583631ef6
racheljohn05 <racheljohn556@gmail.com>
2025-04-06 17:29:27 -0400

    dummy memories on page

--- memories/urls.py
+++ memories/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('memories/', views.memoriesMethod, name='memories'),
+    path('<int:id>/', views.show, name = 'memories.show'),
 ]
--- memories/views.py
+++ memories/views.py
@@ -18,4 +18,12 @@ def memoriesMethod(request):
     return render(request, 'memories/index.html',
                   {'template_data': template_data})
 
+def show(request, id):
+    memory = memories[id-1]
+    template_data = {}
+    template_data['title'] = memory['title']
+    template_data['memory'] = memory
+    return render(request, 'memories/show.html',
+                  {'template_data' : template_data})
+
 #this is a comment for github purposes
