752dc3ed1bf3aea7b15ee6f951bae8690f7b2914
Ruhi-P <patelruhi06@gmail.com>
2025-04-07 23:12:32 -0400

    Reapply "create memory (w/o logging in)"
    This reverts commit 790f2849b968c852e02ffba1cf0d5296ab3f84b6.

--- memories/urls.py
+++ memories/urls.py
@@ -3,4 +3,6 @@ from . import views
 urlpatterns = [
     path('memories/', views.index, name='memories'),
     path('<int:id>/', views.show, name = 'memories.show'),
+    path('memories/create/', views.create_memory,
+        name='memories.create')
 ]
