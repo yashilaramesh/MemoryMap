907a6264891cc6926038eb03b53f56a80cbee48e
Vaidehi (Vidhi) Gupta <vaidehi.gupta@outlook.com>
2025-04-17 16:35:29 -0400

    Made models for memories

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import User
-
 # Create your models here.
 class CustomUser(AbstractUser):
     # user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -9,6 +8,6 @@ class CustomUser(AbstractUser):
     securityQ2 = models.CharField(max_length=250)
     securityA1 = models.CharField(max_length=250)
     securityA2 = models.CharField(max_length=250)
-    
+    business = models.BooleanField(default=False)
     def __str__(self):
         return self.username
--- memories/models.py
+++ memories/models.py
@@ -2,9 +2,12 @@ from django.db import models
 class Memory(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255)
-    date = models.IntegerField()
-    description = models.TextField()
-    #image = models.ImageField(upload_to='movie_images/')
+    date = models.DateField(default = "2025-4-17")
+    description = models.TextField(default="Memory")
+    latitude = models.FloatField(default=0.0)
+    longitude = models.FloatField(default=0.0)
+    visibility = models.BooleanField(default=False)
+    image = models.ImageField(upload_to='memory_images/', default='memory_images/memory.jpg')
     def __str__(self):
         return str(self.id) + ' - ' + self.title
 # Create your models here.
